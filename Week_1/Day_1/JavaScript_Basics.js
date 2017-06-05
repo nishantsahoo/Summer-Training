377d9ac60e8460ab9e40a6265b130a6849332f64
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-06-05 13:49:09 +0530

    Js_Objects

--- Week_1/Day_1/JavaScript_Basics.js
+++ Week_1/Day_1/JavaScript_Basics.js
@@ -1,4 +1,5 @@
 // All of these statements have been executed on the console in the browser // ctrl+shift+i in chrome and then click on console
+// Js is a functional language
 
 console.log("Hello, world");
 // >Hello, world
@@ -7,7 +8,23 @@ var a = 10;
 // returns undefined in the console
 typeof a
 // >"number"
-a = "SomeString"
+var a = "SomeString"
 // >"SomeString"
 typeof a
 // >"string"
+
+typeof undefined
+// >"undefined"
+typeof null
+// >"object"
+null
+// >null
+
+var b = [1,2,3,4]
+// >Array [ 1, 2, 3, 4]
+typeof b
+// >"object"
+b[0]
+// >1
+b[2]
+// >3
---
+++ Week_1/Day_1/JavaScript_Objects.js
@@ -0,0 +1,2 @@
+// Objects in Js are more like HashMaps/Dictionaries
+// in Js, objects can be created without defining a class
