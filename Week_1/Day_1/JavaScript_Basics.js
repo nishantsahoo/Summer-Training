673cc00da63a98c058aa505c4f39016540f0d0d1
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-06-05 13:43:01 +0530

    Day_1

--- Week_1/Day_1/JavaScript_Basics.js
+++ Week_1/Day_1/JavaScript_Basics.js
@@ -1,1 +1,13 @@
-console.log(" ");
+// All of these statements have been executed on the console in the browser // ctrl+shift+i in chrome and then click on console
+
+console.log("Hello, world");
+// >Hello, world
+var a = 10;
+// >undefined
+// returns undefined in the console
+typeof a
+// >"number"
+a = "SomeString"
+// >"SomeString"
+typeof a
+// >"string"
